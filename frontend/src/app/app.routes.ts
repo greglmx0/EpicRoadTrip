eb7b934c5db3ab2ad496ebd0eda4d3ff8573ccf6
UDeficit <gergoire.lamoureux@epitech.eu>
2024-04-29 02:05:17 +0200

    feat: add login and register routes in app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../view/login/login.component';
+import { RegisterComponent } from '../view/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
