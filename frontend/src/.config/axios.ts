13df1db875c2f2c6476ff369f55e676def74ac80
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-05-13 17:29:22 +0200

    feat: Add environment.dev.ts file and axios configuration for API calls

---
+++ frontend/src/.config/axios.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { environment } from '../environments/environment.dev';
+
+console.log('environment.apiUrl', environment.apiUrl);
+
+const axiosInstance = axios.create({
+  baseURL: environment.apiUrl,
+  withCredentials: true,
+
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+});
+
+export default axiosInstance;
