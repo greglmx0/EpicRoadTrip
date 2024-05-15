77f1677f0513b3b6455897f51852aa5db7d877fe
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-05-15 23:28:58 +0200

    chore: Update .gitignore to exclude environment.dev.ts and environment.prod.ts and add environment.exemple.ts

---
+++ frontend/src/environments/environment.exemple.ts
@@ -0,0 +1,8 @@
+// Dans src/environments/environment.ts
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5000',
+  mapbox: {
+    accessToken: 'your_mapbox_access_token',
+  },
+};
