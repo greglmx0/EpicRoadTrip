77e0e40fee8b943d71b9e9017cd646bf50183eca
UDeficit <gergoire.lamoureux@epitech.eu>
2024-05-20 00:13:56 +0200

    feat: add tab-home component and update home component to include tab-home component

---
+++ frontend/src/app/components/tab-home/tab-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './tab-home.component.html',
+  styleUrl: './tab-home.component.scss'
+})
+export class TabHomeComponent {
+
+}
--- frontend/src/view/home/home.component.ts
+++ frontend/src/view/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarHomeComponent } from 'src/app/components/navbar-home/navbar-home.component';
 import { MapComponent } from 'src/app/components/map/map.component';
+import { TabHomeComponent } from 'src/app/components/tab-home/tab-home.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarHomeComponent, MapComponent],
+  imports: [NavbarHomeComponent, MapComponent, TabHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
