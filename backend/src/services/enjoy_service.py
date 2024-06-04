51e6599d4d2fbd98dd2a64dce3799e18e29a44b6
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-06-04 16:41:10 +0200

    chore: Add Ticketmaster API integration and access token to environment variables

--- backend/src/services/enjoy_service.py
+++ backend/src/services/enjoy_service.py
@@ -3,33 +3,21 @@ from flask import jsonify
 import requests
 import os
 
-PREDICTHQ_ACCESS_TOKEN = os.environ.get('PREDICTHQ_ACCESS_TOKEN')
-PREDICTHQ_API_URL = os.environ.get('PREDICTHQ_API_URL') or 'no credentials found'
-
+TICKETMASTER_API_URL= os.getenv('TICKETMASTER_API_URL')
+TICKETMASTER_ACCESS_TOKEN= os.getenv('TICKETMASTER_ACCESS_TOKEN')
 
 class enjoy_service:
     def get_enjoy(latitute, longitude, start_date, end_date):
         try:
             print("enjoy_service.get_enjoy")
             print("latitute: ", latitute, "longitude: ", longitude, "start_date: ", start_date, "end_date: ", end_date)
-            url = f"{PREDICTHQ_API_URL}/events?location_around.origin={latitute},{longitude}&end.gte={start_date}&end.lte={end_date}"
+            url = f"{TICKETMASTER_API_URL}/events.json?apikey={TICKETMASTER_ACCESS_TOKEN}&latlong={longitude},{latitute}&locale=fr"
             print("url: ", url)
             payload={}
-            headers = {
-                'Authorization': f'Bearer {PREDICTHQ_ACCESS_TOKEN}'
-            }
+            headers = {}
             response = requests.get(url, headers=headers, data=payload)
             # print(response.text)
             return response.text
 
         except Exception as e:
             return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
-
-# import requests
-# url = "https://api.predicthq.com/v1/events?location_around.origin=48.1173,-1.6778&end.gte=2024-05-01&end.lte=2024-05-02"
-# payload={}
-# headers = {
-#   'Authorization': 'Bearer FCwEWQJizazDyimtOXw6bKah3aMMs38PiKS0CqqE'
-# }
-# response = requests.request("GET", url, headers=headers, data=payload)
-# print(response.text)
