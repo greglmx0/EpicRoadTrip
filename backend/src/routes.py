af13eb5e04d05db1dfcf288d082934b83152db15
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-07-22 09:52:38 +0200

    feat: add eat_controller and eat_service for retrieving places to eat

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from src.controllers.user_controller import user_controller
 from src.controllers.enjoy_controller import enjoy_controller
 from src.controllers.mapbox_controller import mapbox_controller
+from src.controllers.eat_controller import eat_controller
 
 routes = Blueprint("routes", __name__)
 @routes.route("/")
@@ -10,4 +11,5 @@ def hello_world():
 
 routes.register_blueprint(user_controller)
 routes.register_blueprint(enjoy_controller)
+routes.register_blueprint(eat_controller)
 routes.register_blueprint(mapbox_controller)
---
+++ backend/src/services/eat_service.py
@@ -0,0 +1,25 @@
+from flask import jsonify
+import requests
+import os
+
+FOURSQUARE_API_URL = os.getenv('FOURSQUARE_API_URL')
+FOURSQUARE_API_KEY = os.getenv('FOURSQUARE_API_KEY')
+
+class eat_service:
+    def get_eat(latitute, longitude, start_date, end_date):
+        try:
+            print("eat_service.get_eat")
+            print("latitute: ", latitute, "longitude: ", longitude, "start_date: ", start_date, "end_date: ", end_date)
+            # 13003 is the category id for Dining and Drinking > Restaurant
+            url = f"{FOURSQUARE_API_URL}/places/search?categories=13003"
+            print("url: ", url)
+            payload={}
+            headers = {
+                'Authorization': FOURSQUARE_API_KEY
+            }
+            response = requests.get(url, headers=headers, data=payload)
+            # print(response.text)
+            return response.text
+
+        except Exception as e:
+            return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
