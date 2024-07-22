3d9935229c766e675895562f59c7790e3b7005a0
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-07-22 12:29:44 +0200

    refactor: getEnjoy method in ApiEnjoy class

--- frontend/src/api/enjoy.ts
+++ frontend/src/api/enjoy.ts
@@ -3,15 +3,10 @@ import type { Event } from './dto/enjoy.dto';
 import EnjoyDto from './dto/enjoy.dto';
 
 class ApiEnjoy {
-  static async getEnjoy(
-    latitute: number,
-    longitude: number,
-    start_date: string,
-    end_date: string
-  ) {
+  static async getEnjoy(latitute: number, longitude: number, start_date: string, end_date: string) {
     try {
       const responce = await axiosInstance.get(
-        `/enjoy?latitute=${latitute}&longitude=${longitude}&start_date=${start_date}&end_date=${end_date}`
+        `/enjoy?latitute=${latitute}&longitude=${longitude}&start_date=${start_date}&end_date=${end_date}`,
       );
       const enjoy = ApiEnjoy.convertEnjoyDto(responce.data._embedded.events);
       return enjoy;
