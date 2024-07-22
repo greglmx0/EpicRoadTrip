0ae1f3daf5e53394fde2b9e7fb788dbc3f08a649
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-07-22 12:29:18 +0200

    feat: add ApiDrink class for retrieving drink data

---
+++ frontend/src/api/ApiDrink.ts
@@ -0,0 +1,28 @@
+import axiosInstance from '../.config/axios';
+import type { Event } from './dto/drink.dto';
+import DrinkDto from './dto/drink.dto';
+
+class ApiDrink {
+  static async getDrink(latitute: number, longitude: number, start_date: string, end_date: string) {
+    try {
+      const responce = (await axiosInstance.get(
+        `/drink?latitute=${latitute}&longitude=${longitude}&start_date=${start_date}&end_date=${end_date}`,
+      )) as any;
+      const drink = ApiDrink.convertDrinkDto(responce.data?.results as any);
+      console.log('drink', drink);
+
+      return drink;
+    } catch (error: any) {
+      return {
+        status: error.response.status,
+        data: { message: error.response.data.message },
+      };
+    }
+  }
+
+  private static convertDrinkDto(data: Event[]) {
+    return data.map((drink: Event) => new DrinkDto(drink));
+  }
+}
+
+export default ApiDrink;
