e2ce4ce6e3181dc2e36d3f45152ce0561e5715e1
UDeficit <gregoire.lamoureux@epitech.eu>
2024-09-02 23:49:11 +0200

    feat:  init trip with interest points component

--- frontend/src/components/tab-home/tab-home.component.ts
+++ frontend/src/components/tab-home/tab-home.component.ts
@@ -4,11 +4,12 @@ import ApiMapbox from '../../api/mapbox';
 import { debounceTime, Subject } from 'rxjs';
 import { FormsModule } from '@angular/forms';
 import { SearchPlacesOfInterestComponent } from '../search-places-of-interest/search-places-of-interest.component';
+import { TripWithInterestPointsContainerComponent } from '../trip-with-interest-points-container/trip-with-interest-points-container.component';
 
 @Component({
   selector: 'app-tab-home',
   standalone: true,
-  imports: [CommonModule, FormsModule, SearchPlacesOfInterestComponent],
+  imports: [CommonModule, FormsModule, SearchPlacesOfInterestComponent, TripWithInterestPointsContainerComponent],
   templateUrl: './tab-home.component.html',
   styleUrl: './tab-home.component.scss',
 })
---
+++ frontend/src/components/trip-with-interest-points-container/trip-with-interest-points-container.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-with-interest-points-container',
+  standalone: true,
+  templateUrl: './trip-with-interest-points-container.component.html',
+  styleUrl: './trip-with-interest-points-container.component.scss',
+})
+
+export class TripWithInterestPointsContainerComponent {
+  
+}
