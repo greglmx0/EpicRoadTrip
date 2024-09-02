3533b9e642168ad5835f4fbb17bc882b03cfbc7c
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2024-09-02 11:55:01 +0200

    feat: add popup on map

--- frontend/src/components/map/map.component.ts
+++ frontend/src/components/map/map.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { environment } from '../../environments/environment';
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { ApplicationRef, Component, Input, OnInit } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 @Component({
   selector: 'app-map',
@@ -12,16 +13,18 @@ import mapboxgl from 'mapbox-gl';
 })
 export class MapComponent implements OnInit {
   map: mapboxgl.Map | undefined;
-  @Input() points: Array<{ name: string; coordinates: number[] }> = [];
-  @Input() lat: number = 0;
-  @Input() lng: number = 0;
-  style = 'mapbox://styles/mapbox/streets-v11';
+  @Input() points: Array<{ name: string; coordinates: number[]; description?: string }> = [];
+  @Input() lat: number = 48.1113;
+  @Input() lng: number = -1.6774;
+  style = 'mapbox://styles/mapbox/streets-v12';
+
+  constructor() {}
 
   ngOnInit() {
     this.map = new mapboxgl.Map({
       accessToken: environment.mapbox.accessToken,
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v12',
+      style: this.style,
       zoom: 11,
       center: [this.lng, this.lat],
     });
@@ -40,6 +43,7 @@ export class MapComponent implements OnInit {
               },
               properties: {
                 title: point.name,
+                description: point.name,
               },
             };
           }) as any,
@@ -48,8 +52,8 @@ export class MapComponent implements OnInit {
 
       this.map?.addLayer({
         id: 'Rennes Points',
-        type: 'circle', // circle marker types
-        source: 'Rennes Points', // reference the data source
+        type: 'circle',
+        source: 'Rennes Points',
         layout: {},
         paint: {
           'circle-color': 'blue',
@@ -57,6 +61,30 @@ export class MapComponent implements OnInit {
           'circle-stroke-width': 2,
           'circle-stroke-color': 'white',
         },
+      });
+
+      const popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false,
+      });
+
+      this.map?.on('mouseenter', 'Rennes Points', (e: any) => {
+        if (!this.map) {
+          console.log('map is not here');
+        }
+        this.map!.getCanvas().style.cursor = 'pointer';
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        popup.setLngLat(coordinates).setHTML(description).addTo(this.map!);
+      });
+
+      this.map?.on('mouseleave', 'Rennes Points', () => {
+        this.map!.getCanvas().style.cursor = '';
+        popup.remove();
       });
     });
   }
