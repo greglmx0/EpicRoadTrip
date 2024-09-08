aac45eea0a89c28b6cf649c9c95765b17fa0b6eb
UDeficit <gregoire.lamoureux@epitech.eu>
2024-09-08 19:31:54 +0200

    feat: add mapbox route trip

--- backend/src/services/mapbox_service.py
+++ backend/src/services/mapbox_service.py
@@ -12,7 +12,7 @@ class mapbox_service:
             print("mapbox_service.get_suggest")
             print("query: ", query)
             # url exemple https://api.mapbox.com/search/searchbox/v1/suggest?q={search_text}&access_token={access_token}
-            url = f"{MPABOX_API_URL}/suggest?q={query}&access_token={MAPBOX_ACCESS_TOKEN}&session_token={SESSION_TOKEN}"
+            url = f"{MPABOX_API_URL}/search/searchbox/v1/suggest?q={query}&access_token={MAPBOX_ACCESS_TOKEN}&session_token={SESSION_TOKEN}"
             # print("url: ", url)
             payload={}
             headers = {}
@@ -20,16 +20,32 @@ class mapbox_service:
             return response.json()['suggestions']
         except Exception as e:
             return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
-        
+
     def get_retrieve(mapbox_id):
         try:
             print("mapbox_service.retrieve")
             print("mapbox_id: ", mapbox_id)
-            url = f"{MPABOX_API_URL}/retrieve/{mapbox_id}?access_token={MAPBOX_ACCESS_TOKEN}&session_token={SESSION_TOKEN}"
+            url = f"{MPABOX_API_URL}/search/searchbox/v1/retrieve/{mapbox_id}?access_token={MAPBOX_ACCESS_TOKEN}&session_token={SESSION_TOKEN}"
             # print("url: ", url)
             payload={}
             headers = {}
             response = requests.get(url, headers=headers, data=payload)
             return response.json()['features']
+        except Exception as e:
+            return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
+
+    def get_trip(depart_lat, depart_lon, arrive_lat, arrive_lon):
+        try:
+            print(f'mapbox_service.get_trip  depart: {depart_lat}, {depart_lon} arrive: {arrive_lat}, {arrive_lon}')
+            # https://api.mapbox.com/directions/v5/mapbox/driving/-1.681558,48.113247;2.348392,48.853495?alternatives=true&geometries=geojson&language=fr&overview=full&steps=true&access_token=
+            url = f"{MPABOX_API_URL}/directions/v5/mapbox/driving/{depart_lat},{depart_lon};{arrive_lat},{arrive_lon}?alternatives=true&geometries=geojson&language=fr&overview=full&steps=true&access_token={MAPBOX_ACCESS_TOKEN}"
+            # print("url: ", url)
+            payload={}
+            headers = {}
+            response = requests.get(url, headers=headers, data=payload)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return jsonify({'message': 'An error occurred', 'error': response.text}), 500
         except Exception as e:
             return jsonify({'message': 'An error occurred', 'error': str(e)}), 500
