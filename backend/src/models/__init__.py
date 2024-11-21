d764e5386d6cbbdd33006db5def2aaa5d8f354a0
UDeficit <gregoire.lamoureux@epitech.eu>
2024-11-21 10:12:01 +0100

    fix: reorder import statements in models initialization

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,6 +1,5 @@
-from src.config.database import db
-
 from src.models.activity_model import Activity
 from src.models.trip_model import Trip
 from src.models.user_model import User
 
+from src.config.database import db
